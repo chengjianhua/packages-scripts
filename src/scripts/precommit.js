97ec68d41817d4f723d995fe931f1a03a50b5610
Jianhua Cheng <w.chengjianhua@gmail.com>
2018-12-03 10:31:00 +0800

    perf: load lint-staged configuratin through cosmiconfig

--- src/scripts/precommit.js
+++ src/scripts/precommit.js
@@ -1,27 +1,38 @@
 const path = require('path')
 const spawn = require('cross-spawn')
-const {isOptedIn, hasPkgProp, hasFile, resolveBin} = require('../utils')
+// const lintStaged = require('lint-staged/src/index')
+const cosmiconfig = require('cosmiconfig')
+
+const {isOptedIn, resolveBin, appDirectory} = require('../utils')
 
 const here = p => path.join(__dirname, p)
 const hereRelative = p => here(p).replace(process.cwd(), '.')
 
 const args = process.argv.slice(2)
 
-const useBuiltInConfig =
-  !args.includes('--config') &&
-  !hasFile('.lintstagedrc') &&
-  !hasFile('lint-staged.config.js') &&
-  !hasPkgProp('lint-staged')
+const lintStagedConfigSearchResult = loadLintStagedConfig()
+// eslint-disable-next-line no-unused-vars
+let useBuiltInConfig, configFilePath
 
-const config = useBuiltInConfig
-  ? ['--config', hereRelative('../config/lintstagedrc.js')]
-  : []
+if (lintStagedConfigSearchResult) {
+  configFilePath = lintStagedConfigSearchResult
+    ? lintStagedConfigSearchResult.filepath
+    : (useBuiltInConfig = false)
+} else {
+  configFilePath = require.resolve('../config/lintstagedrc.js')
+  useBuiltInConfig = true
+}
 
-const lintStagedResult = spawn.sync(
-  resolveBin('lint-staged'),
-  [...config, ...args],
-  {stdio: 'inherit'},
-)
+useBuiltInConfig = useBuiltInConfig && !args.includes('--config')
+// lintStaged(console, configFilePath)
+
+if (useBuiltInConfig) {
+  args.push('--config', hereRelative('../config/lintstagedrc.js'))
+}
+
+const lintStagedResult = spawn.sync(resolveBin('lint-staged'), args, {
+  stdio: 'inherit',
+})
 
 if (lintStagedResult.status !== 0 || !isOptedIn('pre-commit')) {
   process.exit(lintStagedResult.status)
@@ -31,4 +42,26 @@ if (lintStagedResult.status !== 0 || !isOptedIn('pre-commit')) {
   })
 
   process.exit(validateResult.status)
+}
+
+/**
+ * try to load `lint-staged` configuration manually, wait https://github.com/okonet/lint-staged/pull/551
+ * to be merged
+ *
+ * @return {*} lintStagedConfigSearchResult
+ */
+function loadLintStagedConfig() {
+  const explorer = cosmiconfig('lint-staged', {
+    searchPlaces: [
+      'package.json',
+      '.lintstagedrc',
+      '.lintstagedrc.json',
+      '.lintstagedrc.yaml',
+      '.lintstagedrc.yml',
+      '.lintstagedrc.js',
+      'lint-staged.config.js',
+    ],
+  })
+
+  return explorer.searchSync(appDirectory)
 }
